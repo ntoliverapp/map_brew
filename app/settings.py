495fe27a2a8563128a880fad9c92c0e264390b41
ntoliverapp <n.toliverapplication@gmail.com>
2022-01-06 07:32:51 -0800

    updating front end

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,7 @@ import re
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # from decouple import config
 # SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
 # SECRET_KEY = config('SECRET_KEY')
